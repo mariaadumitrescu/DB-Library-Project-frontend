f898ff15906688cfdc8b07c163cf96b0434a34b1
Brebu <brebu.ciprian@gmail.com>
2019-09-06 10:27:21 +0300

    Refactoring + Memory list

--- src/app/pages/grid-books/book-grid-icon/book-grid-icon.component.ts
+++ src/app/pages/grid-books/book-grid-icon/book-grid-icon.component.ts
@@ -1,13 +1,14 @@
-import { Component, OnInit, Input } from '@angular/core';
-import { Book } from 'src/app/models/book';
+import {Component, OnInit, Input} from '@angular/core';
+import {Book} from 'src/app/models/book';
 import {BookService} from '../../../services/book.service';
-import { Router } from '@angular/router';
+import {Router} from '@angular/router';
 import {AuthenticationService} from '../../../services/autentication.service';
 import * as jwt_decode from 'jwt-decode';
 import {UserService} from '../../../services/user.service';
 import {UserBook} from '../../../models/userBook';
 import {UserBookService} from '../../../services/userBook.service';
 import {getMilliseconds} from 'ngx-bootstrap/chronos/utils/date-getters';
+import {ToastrService} from 'ngx-toastr';
 
 @Component({
   selector: 'book-grid-icon',
@@ -20,7 +21,12 @@ export class BookGridComponent implements OnInit {
   averagePercent: number;
   decoded: any;
 
-  constructor(private userBookService: UserBookService, private userService: UserService, private router: Router, private authenticationService: AuthenticationService) {
+  constructor(private userBookService: UserBookService,
+              private userService: UserService,
+              private router: Router,
+              private authenticationService: AuthenticationService,
+              private toastrService:ToastrService
+  ) {
   }
 
   ngOnInit(): void {
@@ -45,7 +51,21 @@ export class BookGridComponent implements OnInit {
 
     today.setDate(today.getDate() + maximumBorrowedDays);
 
-    await this.userBookService.addUserBook(currentUser, this.book, today.toISOString().slice(0, 10)).subscribe(
-      value => console.log(value)
+    this.userBookService.addUserBook(currentUser, this.book, today.toISOString().slice(0, 10)).subscribe(
+      value => {
+        this.showSuccess();
+      },error => {
+        this.showError(error);
+      }
     );
-  }}
+  }
+
+
+  showSuccess() {
+    this.toastrService.success('The book with title: '+this.book.title+' was borrowed with success!', 'Enjoy!');
+  }
+
+  showError(error:string) {
+    this.toastrService.error(error, 'Error!');
+  }
+}
